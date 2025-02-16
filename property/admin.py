1bfb0d25677ae4af9c287788b0c13ccf6b22593f
Izdator <evarborisov@gmail.com>
2025-02-16 15:36:39 +0300

    Update admin.py
    Apartment owners are shown.

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    extra = 1
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owners__name']
     readonly_fields = ('created_at',)
@@ -15,6 +21,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     list_editable = ('new_building',)
     raw_id_fields = ('liked_by',)
+    inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
@@ -34,3 +41,4 @@ class OwnerAdmin(admin.ModelAdmin):
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
 admin.site.register(Owner, OwnerAdmin)
+                    
